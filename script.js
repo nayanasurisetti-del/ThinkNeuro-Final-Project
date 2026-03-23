ed5d6cd34332b2502c14f20534baca40b748bc22
.Nayana <nayanasurisetti@gmail.com>
2026-03-22 18:40:33 -0700

    fix9

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function happy(){
-    console.log("happy");
+async function happy(){
+    const quote = await getQuote();
+    const display = document.getElementById("moodDisplay");
+    display.innerHTML = `
+    <h3>😊 You selected happy!</h3>
+    <p>${quote}</p>
+    `;
 }
-function stressed(){
-    console.log("Stressed");
+async function stressed(){
+    const quote = await getQuote();
+    const display = document.getElementById("moodDisplay");
+    display.innerHTML = `
+    <h3>😟 You selected stressed</h3>
+    <p>${quote}</p>
+    `;
 }
-function sad(){
-    console.log("sad");
+async function sad(){
+    const quote = await getQuote();
+    const display = document.getElementById("moodDisplay");
+    display.innerHTML = `
+    <h3>🥺 You selected sad</h3>
+    <p>${quote}</p>
+    `;
 }
-
 async function getQuote() {
     const proxy = "https://api.allorigins.win/raw?url=";
     const url = proxy + encodeURIComponent("https://zenquotes.io/api/random");
