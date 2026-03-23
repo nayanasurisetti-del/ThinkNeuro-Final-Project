2b56e21011441cbc519eefd6e24e2967e70276c0
.Nayana <nayanasurisetti@gmail.com>
2026-03-22 17:52:03 -0700

    fix2

--- script.js
+++ script.js
@@ -24,7 +24,9 @@ async function sad(){
 }
 async function getQuote(){
     try{
-        const response = await fetch("https://zenquotes.io/api/random");
+        const response = await fetch(
+        "https://api.allorigins.win/get?url=" + encodeURIComponent("https://zenquotes.io/api/random")
+        );
         const data = await response.json();
         return data[0].q + " - " + data[0].a;
     } catch (error) {
