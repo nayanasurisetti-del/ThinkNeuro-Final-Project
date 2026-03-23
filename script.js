48697a201ac228f9e7437da9e22b080b4a145b3e
.Nayana <nayanasurisetti@gmail.com>
2026-03-22 17:00:15 -0700

    Add quote display

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-function happy(){
-    alert("You clicked happy!");
+async function happy(){
+    const quote = await getQuote();
+    const display = document.getElementById("moodDisplay");
+    display.innerHTML = `
+    <h3>😊 You selected happy!</h3>
+    <p>${quote}</p>
+    `;
 }
-function stressed(){
-    alert("You clicked stressed!");
+async function stressed(){
+    const quote = await getQuote();
+    const display = document.getElementById("moodDisplay");
+    display.innerHTML = `
+    <h3>😟 You selected stressed</h3>
+    <p>${quote}</p>
+    `;
 }
-function sad(){
-    alert("You clicked sad!");
+async function sad(){
+    const quote = await getQuote();
+    const display = document.getElementById("moodDisplay");
+    display.innerHTML = `
+    <h3>🥺 You selected sad</h3>
+    <p>${quote}</p>
+    `;
+}
+async function getQuote(){
+    try{
+        const response = await fetch('https://zenquotes.io/api/random');
+        const data = await response.json();
+        return data[0].q + " - " + data[0].a;
+    } catch (error) {
+        console.log("Error fetching quote:", error);
+        return "Stay positive!";
+    }
 }
